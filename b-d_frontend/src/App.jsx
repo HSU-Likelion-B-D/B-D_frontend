a12d77172cff25650681646115806a778c9561d9
nyun-nye <2001_11_7@naver.com>
2025-08-07 13:41:45 +0900

    ✨: 메인페이지 구조 생성

--- b-d_frontend/src/App.jsx
+++ b-d_frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import MainPage from "./pages/MainPage";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="/" element={<HomePage />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
