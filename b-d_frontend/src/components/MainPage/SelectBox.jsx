e1f0e1d11594082576672c1b24c9cc4ce309d997
nyun-nye <2001_11_7@naver.com>
2025-08-07 14:31:21 +0900

    ✨: SelectBox 생성 완료

--- b-d_frontend/src/components/MainPage/SelectBox.jsx
+++ b-d_frontend/src/components/MainPage/SelectBox.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "@/styles/MainPage/SelectBox.module.scss";
 
-function SelectBox({ title, content }) {
+function SelectBox({ title, content1, content2, style }) {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <span>{title}</span>로 활동하고 싶어요.
       </div>
-      <div className={styles.content}>{content}</div>
+      <div className={styles.content} style={style}>
+        <div className={styles.content1}>{content1}</div>
+        <div className={styles.content2}>{content2}</div>
+      </div>
     </div>
   );
 }
--- b-d_frontend/src/pages/MainPage.jsx
+++ b-d_frontend/src/pages/MainPage.jsx
@@ -5,7 +5,22 @@ import SelectBox from "@/components/MainPage/SelectBox";
 function MainPage() {
   return (
     <div className={styles.container}>
-      <SelectBox title="title" content="content" />
+      <SelectBox
+        title="자영업자"
+        content1="인플루언서와의 공생관계"
+        content2="가게를 홍보할 수 있어요!"
+        style={{
+          textAlign: "end",
+        }}
+      />
+      <SelectBox
+        title="인플루언서"
+        content1="협찬을 통한"
+        content2="여러 콜라보 활동을 할 수 있어요!"
+        style={{
+          textAlign: "start",
+        }}
+      />
     </div>
   );
 }
