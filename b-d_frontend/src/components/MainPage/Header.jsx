8cdd76d4ce0fd8c434a44609eda33dcd0c069590
nyun-nye <2001_11_7@naver.com>
2025-08-13 21:13:44 +0900

    ✨: Header 컴포넌트 구현

---
+++ b-d_frontend/src/components/MainPage/Header.jsx
@@ -0,0 +1,49 @@
+import styles from "@/styles/components/Header.module.scss";
+import {
+  logo,
+  alarm_icon,
+  chat_icon,
+  profile_icon,
+  hamburger_icon,
+} from "@/assets";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.iconContainer}>
+        <div className={styles.iconBox}>
+          <img className={styles.icon} src={chat_icon} alt="chat" />
+          <div className={styles.count}>1</div>
+        </div>
+        <div className={styles.iconBox}>
+          <img className={styles.icon} src={alarm_icon} alt="alarm" />
+          <div className={styles.count}>1</div>
+        </div>
+      </div>
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>
+      <div className={styles.rightIconContainer}>
+        <div className={styles.userInfoContainer}>
+          <div className={styles.userNameContainer}>
+            <span className={styles.userName}>화이팅</span> 님
+          </div>
+          <div className={styles.profileContainer}>
+            <img
+              className={styles.profileIcon}
+              src={profile_icon}
+              alt="profile"
+            />
+          </div>
+        </div>
+        <div className={styles.hamburgerContainer}>
+          <img
+            className={styles.hamburgerIcon}
+            src={hamburger_icon}
+            alt="hamburger"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
