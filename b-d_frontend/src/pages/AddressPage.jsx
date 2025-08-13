1e820b3ff04b4a5f719801053537e7d82e493d0a
daye <dbk1102@naver.com>
2025-08-13 11:04:50 +0900

    ⚡️ : X 버튼 기능 추가

--- b-d_frontend/src/pages/AddressPage.jsx
+++ b-d_frontend/src/pages/AddressPage.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ProgressBar from "../components/ProfilePage/ProgressBar";
 import styles from "../styles/pages/AddressPage.module.scss";
 import logo from "../assets/logo.svg";
+import Input from "../components/SingupPage/Input";
 const AddressPage = () => {
+  const [formData, setFormData] = useState({
+    address: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    if (name === "address") {
+      setFormData("");
+    }
+  };
+  const clearAddress = () => {
+    setFormData((prevData) => ({
+      ...prevData,
+      address: "",
+    }));
+  };
   return (
     <div className={styles.container}>
       <div className={styles.whiteBox}>
@@ -14,7 +35,38 @@ const AddressPage = () => {
         <p className={styles.description}>
           거의 다왔어요! 비디가 당신을 분석하고 있어요.
         </p>
+        <div className={styles.addressPlaceGroup}>
+          <label htmlFor="nickname" className={styles.label}>
+            사업장<span>*</span>
+          </label>
+          <div className={styles.addressContainer}>
+            <Input
+              id="address"
+              name="address"
+              type="text"
+              placeholder="사업장을 주소를 입력하세요"
+              value={formData.address}
+              showClearButton={true}
+              onChange={handleInputChange}
+              onClear={clearAddress}
+              className={styles.input}
+            />
+            <button type="button" className={styles.searchButton}>
+              검색
+            </button>
+          </div>
+          <Input
+            id="address"
+            name="address"
+            type="text"
+            value={formData.address}
+            placeholder="상세 주소"
+            showClearButton={false}
+            className={styles.detailInput}
+          />
+        </div>
       </div>
+      {/*whiteBox 닫는 div */}
     </div>
   );
 };
