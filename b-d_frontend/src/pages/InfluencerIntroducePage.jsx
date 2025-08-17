b6be8819b056cf78579e29977fd9235eca38bac4
daye <dbk1102@naver.com>
2025-08-17 15:06:55 +0900

    🎨 : placeholder 컬러 변경

--- b-d_frontend/src/pages/InfluencerIntroducePage.jsx
+++ b-d_frontend/src/pages/InfluencerIntroducePage.jsx
@@ -75,9 +75,9 @@ const InfluencerIntroducePage = () => {
           #저를 <span className={styles.highlight}>소개</span>합니다.
         </h1>
         <p className={styles.description}>
-          당신의 사업장을 설명할 수 있는 키워드를 모두 골라주세요.
+          당신을 설명할 수 있는 키워드를 모두 골라주세요.
         </p>
-        <p className={styles.keyDescription}>#이런_일을_하고있어요.</p>
+        <p className={styles.keyDescription}>#제가_활동하는_플랫폼은요</p>
         <div className={styles.inputGroup}>
           <label className={styles.label} style={{ marginBottom: 12 }}>
             활동명<span>*</span>
