6b64c3bd37e21a7a446a29bdc010800c87347789
daye <dbk1102@naver.com>
2025-08-18 19:57:48 +0900

    ✨ : 자영업자 정보 수정 완료 페이지 추가

--- b-d_frontend/src/App.jsx
+++ b-d_frontend/src/App.jsx
@@ -29,6 +29,7 @@ import InfluencerCompletePage from "./pages/InfluencerCompletePage";
 import InfluencerPaymentManagePage from "./pages/InfluencerPaymentManagePage";
 import StoreTimePage from "./pages/StoreTimePage";
 import StoreCostPage from "./pages/StoreCostPage";
+import InfoEditCompletePage from "./pages/InfoEditCompletePage";
 
 function App() {
   return (
@@ -63,6 +64,10 @@ function App() {
           />
           <Route path="/store-time" element={<StoreTimePage />} />
           <Route path="/store-cost" element={<StoreCostPage />} />
+          <Route
+            path="/info-edit-complete"
+            element={<InfoEditCompletePage />}
+          />
         </Route>
         <Route element={<SimpleLayout />}>
           <Route path="/login" element={<LoginPage />} />
---
+++ b-d_frontend/src/pages/InfoEditCompletePage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../styles/pages/InfoEditCompletePage.module.scss";
+import { logo, complete_icon } from "@/assets";
+const InfoEditCompletePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.whiteBox}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <h1 className={styles.subtitle}>정보수정을 완료했어요!</h1>
+
+        <p className={styles.description}>비디의 바다에서 펼쳐질 활동들을</p>
+        <p className={styles.description}>비디가 응원할게요!</p>
+
+        <img
+          src={complete_icon}
+          className={styles.completeIcon}
+          alt="complete icon"
+        />
+
+        <button type="submit" className={styles.submitBtn}>
+          다음으로
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default InfoEditCompletePage;
