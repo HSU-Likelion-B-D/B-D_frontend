49ff09c9dca88d2d7a1bb638c45c48914d3fcf7c
nyun-nye <2001_11_7@naver.com>
2025-08-24 02:02:45 +0900

    ✨: 평가 완료 후 새로고침

--- b-d_frontend/src/pages/InfluencerPaymentManagePage.jsx
+++ b-d_frontend/src/pages/InfluencerPaymentManagePage.jsx
@@ -22,12 +22,22 @@ const InfluencerPaymentManagePage = () => {
   const [totalPages, setTotalPages] = useState(0);
   const [paymentList, setPaymentList] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null); // 선택된 아이템 상태 추가
+  const [prevModalState, setPrevModalState] = useState(false); // 이전 모달 상태 추적
   // 모달이 열릴 때 스크롤을 최상단으로 이동
   useEffect(() => {
     if (isPaymentProgressModalOpen || isPaymentCompleteModalOpen) {
       window.scrollTo({ top: 0 });
     }
   }, [isPaymentProgressModalOpen, isPaymentCompleteModalOpen]);
+
+  // 정산 완료 모달이 닫힐 때 새로고침
+  useEffect(() => {
+    if (prevModalState && !isPaymentCompleteModalOpen) {
+      // 이전에 모달이 열려있었고, 지금 닫혔을 때만 새로고침
+      window.location.reload();
+    }
+    setPrevModalState(isPaymentCompleteModalOpen);
+  }, [isPaymentCompleteModalOpen, prevModalState]);
 
   // 현재 페이지에 보여줄 데이터만 추출 - 6개의 데이터만
   const pagedList = paymentList.slice(
