a616db4dd2927008c656c364faf32bde3a17a232
nyun-nye <2001_11_7@naver.com>
2025-08-24 23:15:30 +0900

    ✨: 새로고침, 제안서 확인 버튼 동작 구현

--- b-d_frontend/src/pages/InfluencerMatchingPage.jsx
+++ b-d_frontend/src/pages/InfluencerMatchingPage.jsx
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 export default function InfluencerMatchingPage() {
   const [isProposalModalOpen, setIsProposalModalOpen] = useState(false);
   const [recommendations, setRecommendations] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
   const navigate = useNavigate();
 
   const proposalId = sessionStorage.getItem("proposalId"); // createproposal 할때 저장함
@@ -20,11 +21,20 @@ export default function InfluencerMatchingPage() {
     }
   }, [isProposalModalOpen]);
 
-  useEffect(() => {
-    axiosInstance.get("/bd/api/businessman/me/recommendations").then((res) => {
+  const fetchRecommendations = async () => {
+    try {
+      const res = await axiosInstance.get(
+        "/bd/api/businessman/me/recommendations"
+      );
       setRecommendations(res.data.data);
       console.log(res.data);
-    });
+    } catch (error) {
+      console.error("추천 데이터 가져오기 실패:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecommendations();
   }, []);
 
   return (
@@ -32,7 +42,12 @@ export default function InfluencerMatchingPage() {
       <div className={styles.content}>
         <Header />
         <div className={styles.topContainer}>
-          <button className={styles.myProposalButton}>제안서 확인</button>
+          <button
+            className={styles.myProposalButton}
+            onClick={() => navigate("/create-proposal")}
+          >
+            제안서 확인
+          </button>
           <div className={styles.titleContainer}>
             <div className={styles.title}>
               <span>인플루언서 </span>매칭하기
@@ -40,7 +55,19 @@ export default function InfluencerMatchingPage() {
             <div className={styles.subtitle}>사장님을 기다리고 있어요!</div>
           </div>
 
-          <button className={styles.refreshButton}>새로고침</button>
+          <button
+            className={styles.refreshButton}
+            disabled={isRefreshing}
+            onClick={async () => {
+              if (!isRefreshing) {
+                setIsRefreshing(true);
+                await fetchRecommendations();
+                setIsRefreshing(false);
+              }
+            }}
+          >
+            새로고침
+          </button>
         </div>
 
         <div className={styles.description}>
