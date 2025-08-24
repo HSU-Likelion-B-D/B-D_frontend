7e47fa90dbe067b23dfa8d282f5aeb51f065ea82
nyun-nye <2001_11_7@naver.com>
2025-08-24 13:19:58 +0900

    🐛: 인플루언서 메인페이지 반응형 수정

--- b-d_frontend/src/components/InfluencerMainPage/MatchingItem.jsx
+++ b-d_frontend/src/components/InfluencerMainPage/MatchingItem.jsx
@@ -3,12 +3,14 @@ import { profile_img } from "@/assets";
 export default function MatchingItem({ business }) {
   return (
     <div className={styles.container}>
-      {business.imgUrl ? (
-        <img src={business.imgUrl} className={styles.profileImg} />
-      ) : (
-        <img src={profile_img} className={styles.profileImg} />
-      )}
-      <div className={styles.name}>{business.nickName}</div>
+      <div className={styles.leftContainer}>
+        {business.imgUrl ? (
+          <img src={business.imgUrl} className={styles.profileImg} />
+        ) : (
+          <img src={profile_img} className={styles.profileImg} />
+        )}
+        <div className={styles.name}>{business.nickName}</div>
+      </div>
       <div className={styles.followCount}>
         {business.avgScore} ({business.reviewCount})
       </div>
