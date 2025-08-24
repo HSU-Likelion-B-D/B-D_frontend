6bd5cfc5f186079c08d462a81fec0306d53edd57
nyun-nye <2001_11_7@naver.com>
2025-08-24 11:18:58 +0900

    🐛: 메인페이지 반응형 스타일 버그 수정

--- b-d_frontend/src/components/MainPage/MatchingItem.jsx
+++ b-d_frontend/src/components/MainPage/MatchingItem.jsx
@@ -3,12 +3,14 @@ import { influencer_profile } from "@/assets";
 export default function MatchingItem({ influencer }) {
   return (
     <div className={styles.container}>
-      {influencer.imgUrl ? (
-        <img src={influencer.imgUrl} className={styles.profileImg} />
-      ) : (
-        <img src={influencer_profile} className={styles.profileImg} />
-      )}
-      <div className={styles.name}>{influencer.nickName}</div>
+      <div className={styles.leftContainer}>
+        {influencer.imgUrl ? (
+          <img src={influencer.imgUrl} className={styles.profileImg} />
+        ) : (
+          <img src={influencer_profile} className={styles.profileImg} />
+        )}
+        <div className={styles.name}>{influencer.nickName}</div>
+      </div>
       <div className={styles.followCount}>{influencer.follower}</div>
     </div>
   );
